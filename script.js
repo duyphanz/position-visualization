585a8f70542847ad9ca00621e4ca6126648dbe75
Duy Phan <duyphan@lecle.co.kr>
2020-08-28 07:52:13 +0700

    update handle scolling

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const horizontalLine = document.querySelector('.app__line--horizontal')
 const verticalLine = document.querySelector('.app__line--vertical')
 
+let PageY = 0;
+let ClientY = 0
 
 document.addEventListener('mousemove', event => {
   const { clientX, clientY, pageX, pageY } = event;
+
+  PageY = pageY;
+  ClientY = clientY;
 
   horizontalLine.style.top = clientY < 100 ? clientY: clientY + 'px';
   horizontalLine.style.width = clientX + 'px';
@@ -11,7 +16,13 @@ document.addEventListener('mousemove', event => {
 
   verticalLine.style.left = clientX + 'px';
   verticalLine.style.height = clientY < 100 ? clientY: clientY + 'px';
-  verticalLine.childNodes[1].innerHTML = 'pageY:' + pageY;
+  verticalLine.childNodes[1].innerHTML = 'pageY:' + PageY;
   verticalLine.childNodes[3].innerHTML = '--clientY:' + (clientY - 100);
 })
 
+
+
+document.addEventListener('scroll', event => {
+  const scroll = this.scrollY;
+  verticalLine.childNodes[1].innerHTML = 'pageY:' + (ClientY + scroll);
+})
