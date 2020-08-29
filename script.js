8e47f3463285b0e7b45b6a7c7179b3bf0370ad36
Duy Phan <duyphan@lecle.co.kr>
2020-08-29 15:02:12 +0700

    feat: :sparkles: added bottom statistic

--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 let PageY = 0;
 let ClientY = 0;
 let draggingTop = 0;
+let draggingBottom = 0;
 const topSimulatedCover = 100;
 
 // lines of mousemove event
@@ -11,9 +12,12 @@ const verticalLine = document.querySelector('.app__line--vertical');
 
 // lines of dragging element
 const draggingEl = document.querySelector('.app__dragging-element');
-const draggingVerticalLine = draggingEl.childNodes[3];
-const draggingHorizontalLine = draggingEl.childNodes[5];
-const draggingDimensions = draggingEl.childNodes[7];
+const draggingVerticalLine = draggingEl.childNodes[5];
+const draggingHorizontalLine = draggingEl.childNodes[7];
+const draggingDimensions = draggingEl.childNodes[11];
+
+const draggingVerticalLineBottom = draggingEl.childNodes[15];
+const draggingVerticalLineRight = draggingEl.childNodes[17];
 
 const scrollY = document.querySelector('.app__scroll-label');
 const innerHeight = document.querySelector('.inner-height__label');
@@ -44,15 +48,25 @@ document.addEventListener('mousemove', event => {
 
   const rect = draggingEl.getBoundingClientRect();
   draggingTop = rect.top
+  draggingBottom = rect.bottom
 
   draggingVerticalLine.style.left = rect + 'px';
   draggingVerticalLine.style.top = topSimulatedCover + 'px';
   draggingVerticalLine.style.height = rect.top - topSimulatedCover + 'px';
   draggingVerticalLine.childNodes[1].innerHTML = '--rect.top/y:' + (rect.top - topSimulatedCover);
 
+  draggingVerticalLineBottom.style.left = rect.right + 'px';
+  draggingVerticalLineBottom.style.top = topSimulatedCover + 'px';
+  draggingVerticalLineBottom.style.height = rect.bottom - topSimulatedCover + 'px';
+  draggingVerticalLineBottom.childNodes[1].innerHTML = '--rect.bottom:' + (rect.bottom - topSimulatedCover);
+
   draggingHorizontalLine.style.top = rect.top + 'px';
   draggingHorizontalLine.style.width = rect.left + 'px';
   draggingHorizontalLine.childNodes[1].innerHTML = 'rect.left/x:' + rect.left;
+
+  draggingVerticalLineRight.style.top = rect.bottom + 'px';
+  draggingVerticalLineRight.style.width = rect.right + 'px';
+  draggingVerticalLineRight.childNodes[1].innerHTML = 'rect.right:' + rect.right;
 })
 
 document.addEventListener('scroll', event => {
@@ -60,9 +74,17 @@ document.addEventListener('scroll', event => {
   verticalLine.childNodes[1].innerHTML = 'pageY:' + (ClientY + scroll);
 
   const draggingHeight = draggingTop - topSimulatedCover - scroll;
+  const draggingHeightBottom = draggingBottom - topSimulatedCover - scroll;
+
   draggingVerticalLine.style.height = draggingHeight + 'px';
-  draggingHorizontalLine.style.top = draggingTop - scroll + 'px';
   draggingVerticalLine.childNodes[1].innerHTML = draggingHeight < 0 ? '' : '--rect.top/y:' + draggingHeight;
+  draggingVerticalLineBottom.style.height = draggingHeightBottom + 'px';
+  draggingVerticalLineBottom.childNodes[1].innerHTML = draggingHeightBottom < 0 ? '' : '--rect.bottom:' + draggingHeightBottom;
+
+
+  draggingHorizontalLine.style.top = draggingTop - scroll + 'px';
+  draggingVerticalLineRight.style.top = draggingBottom - scroll + 'px';
+
 
   scrollY.innerHTML = 'scrollY:' + (topSimulatedCover + scroll);
 })
@@ -80,8 +102,6 @@ const mouseDownHandler = function(e) {
   const rect = draggingEl.getBoundingClientRect();
   x = e.pageX - rect.left;
   y = e.pageY - rect.top;
-
-  console.log(draggingDimensions)
 
   draggingDimensions.innerHTML = 'rect.width:' + rect.width + ', rect.height: ' + rect.height;
 
